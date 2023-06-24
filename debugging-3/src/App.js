e4e19051b6159f7706d54d0180230ff33d42de37
Shahid Amin <schboostie@gmail.com>
2023-06-24 15:31:59 +0100

    Update App.js

--- debugging-3/src/App.js
+++ debugging-3/src/App.js
@@ -1,89 +1,98 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function EmailInput() {
-    const [email, setEmail] = useState(null);
+  const [email, setEmail] = useState(null);
 
-    function valid(val) {
-        return val && val.indexOf('@') !== '-1'
-    }
+  function valid(val) {
+    return val && val.indexOf("@") !== "-1";
+  }
 
-    return <div>
-        <label htmlFor='email'>Email:</label>
-        <input
-            onChange={(e) => setEmail(e.target.value)}
-            onBlur={() => {
-                if (!valid(email)) {
-                    alert('Email not valid')
-                }
-            }
-            }
-            id="email" type='text' value={email}/>
+  return (
+    <div>
+      <label htmlFor="email">Email:</label>
+      <input
+        onChange={(e) => setEmail(e.target.value)}
+        onBlur={() => {
+          if (!valid(email)) {
+            alert("Email not valid");
+          }
+        }}
+        id="email"
+        type="text"
+        value={email}
+      />
     </div>
+  );
 }
 
 function SelectTvShow() {
-    const [show, setShow] = useState(84);
-    const [episodes, setEpisodes] = useState([]);
+  const [show, setShow] = useState(84);
+  const [episodes, setEpisodes] = useState([]);
 
-    useEffect(() => {
-        fetch(`https://api.tvmaze.com/show/${show}/episodes`).then((response) => {
-            return response.json()
-        }).then((data) => {
-            setEpisodes(data)
-        }).catch(() => {
+  useEffect(() => {
+    fetch(`https://api.tvmaze.com/shows/${show}/episodes`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const sortedEpisodes = data.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+        setEpisodes(sortedEpisodes);
+      })
+      .catch(() => {});
+  }, []);
 
-        })
-    }, [])
-
-
-    return <div>
-        <label htmlFor='tvshows'>Episode:</label>
-        <select id='tvshows'>
-            <option>Please Select:</option>
-            {episodes && episodes.map((e) => {
-                return <option>{e.name}</option>
-            })}
-        </select>
+  return (
+    <div>
+      <label htmlFor="tvshows">Episode:</label>
+      <select id="tvshows">
+        <option>Please Select:</option>
+        {episodes &&
+          episodes.map((e) => {
+            return <option>{e.name}</option>;
+          })}
+      </select>
     </div>
+  );
 }
 
 function Rating() {
-    const [value, setValue] = useState(null);
-    let values = ['Bad', 'Ok', 'Good', 'Great', 'Amazing']
-    return <div>
-        <label htmlFor='rating'>Rating:</label>
-        <select
-            id="rating"
-            onChange={
-                (e) => {
-                    setValue(e.target.value)
-                    alert(`Rating ${e.target.value}`)
-                }
-            }
-        >
-            <option>Please Select:</option>
-            {values.map((e) => {
-                return <option value={e}>{e}</option>
-            })}
-        </select>
+  const [value, setValue] = useState(null);
+  let values = ["Bad", "Ok", "Good", "Great", "Amazing"];
+  return (
+    <div>
+      <label htmlFor="rating">Rating:</label>
+      <select
+        id="rating"
+        onChange={(e) => {
+          setValue(e.target.value);
+          alert(`Rating ${e.target.value}`);
+        }}
+      >
+        <option>Please Select:</option>
+        {values.map((e) => {
+          return <option value={e}>{e}</option>;
+        })}
+      </select>
     </div>
+  );
 }
 
 function App() {
-    return (
-        <>
-            <form>
-                <h1>Family Guy Episodes</h1>
-                <div className="App">
-                    <EmailInput/>
-                    <SelectTvShow/>
-                    <Rating/>
-                </div>
-            </form>
-        </>
-    )
-        ;
+  return (
+    <>
+      <form>
+        <h1>Family Guy Episodes</h1>
+        <div className="App">
+          <EmailInput />
+          <SelectTvShow />
+          <Rating />
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default App;
